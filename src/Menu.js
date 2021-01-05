eb2f475bfde9ad3c603717ce6f6e57b63ef34fd9
josenauto <jemilionautoch@gmail.com>
2021-01-05 16:49:18 -0300

    Showed title, price and desc

--- src/Menu.js
+++ src/Menu.js
@@ -4,10 +4,17 @@ const Menu = ({ items }) => {
   return (
     <div>
       {items.map((menuItem) => {
-        const { id, title, img, desc } = menuItem;
+        const { id, title, img, desc, price } = menuItem;
         return (
           <div key={id}>
             <img src={img} alt={title} />
+            <div>
+                <header>
+                    <h4>{title}</h4>
+                    <h4>{price}</h4>
+                </header>
+                <p>{desc}</p>
+            </div>
           </div>
         );
       })}
